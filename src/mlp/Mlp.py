70cf941bbe07eb396d7a43760d3b0b9123d9e5cf
Gilmar Júnior <gilmar.junior@virtual360.io>
2025-03-10 18:06:23 -0300

    fix: adjusting input and target functions

--- src/mlp/Mlp.py
+++ src/mlp/Mlp.py
@@ -4,24 +4,26 @@ import matplotlib.pyplot as plt
 
 class Mlp:
 
-    def __init__(number_neurons, sample_size, x_min, x_max, learning_rate):
+    def __init__(self, number_neurons, sample_size, x_min, x_max, learning_rate):
         self.number_neurons = number_neurons
         self.sample_size = sample_size
         self.x_min = x_min
         self.x_max = x_max
         self.learning_rate = learning_rate
-        self.inputs = inputs(x_min, x_max, sample_size)
-        self.targets = targets(self.inputs)
+        self.inputs = self.get_inputs(x_min, x_max, sample_size)
+        self.targets = self.get_targets(self.inputs)
 
-    def inputs(x_min, x_max, sample_size):
+    def get_inputs(self, x_min, x_max, sample_size):
 
         return np.linspace(x_min, x_max, sample_size)
 
-    def targets(inputs):
+    def get_targets(self, inputs):
 
-        targets = []
+        targets = np.array([])
 
         for x in inputs:
-            targets.append(np.sin(x/2)*np.cos(2*x))
+            targets = np.append(targets,np.sin(x/2)*np.cos(2*x))
 
         return targets
+
+        
